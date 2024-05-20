e373baca33a2a97d52beb646bece61f79f3552db
Jennifer11607 <jenniferjimenez152000@gmail.com>
2024-05-20 10:06:40 -0600

    guardar cambios index, buscar y comida

--- modelos/comida.php
+++ modelos/comida.php
@@ -1,9 +1,10 @@
 <?php
 require 'Conexion.php';
 
-class Cliente extends Conexion{
+class Comida extends Conexion{
     public $com_id;
     public $com_nombre;
+    public $com_menu;
     public $com_fecha;
     public $com_tiempo;
     public $com_sirvio;
@@ -12,20 +13,21 @@ class Cliente extends Conexion{
 
     public function __construct($args = [])
     {
-        $this->cli_id = $args['cli_id'] ?? null;
-        $this->cli_nombre = $args['cli_nombre'] ?? '';
-        $this->cli_apellido = $args['cli_apellido'] ?? '';
-        $this->cli_nit = $args['cli_nit'] ?? 0;
-        $this->cli_telefono = $args['cli_telefono'] ?? 0;
-        $this->cli_situacion = $args['cli_situacion'] ?? '';
+        $this->com_id = $args['com_id'] ?? null;
+        $this->com_nombre = $args['com_nombre'] ?? '';
+        $this->com_menu = $args['com_menu'] ?? '';
+        $this->com_fecha = $args['com_fecha'] ?? 0;
+        $this->com_tiempo = $args['com_tiempo'] ?? 0;
+        $this->com_sirvio = $args['com_sirvio'] ?? 0;
+        $this->com_situacion = $args['com_situacion'] ?? '';
 
     }
 
       // METODO PARA INSERTAR
       public function guardar(){
-        $sql = "INSERT into clientes (cli_nombre,
-         cli_apellido, cli_nit, cli_telefono) values ('$this->cli_nombre',
-         '$this->cli_apellido', '$this->cli_nit', '$this->cli_telefono')";
+        $sql = "INSERT into comida (com_nombre,
+         com_menu, com_fecha, com_tiempo, com_sirvio) values ('$this->com_nombre',
+         '$this->com_menu', '$this->com_fecha', '$this->com_tiempo', '$this->com_sirvio')";
         $resultado = $this->ejecutar($sql);
         return $resultado; 
     }
@@ -33,21 +35,24 @@ class Cliente extends Conexion{
 
     public function buscar(...$columnas){
         $cols = count($columnas) > 0 ? implode(',', $columnas) : '*';
-        $sql = "SELECT $cols FROM clientes where cli_situacion = 1 ";
+        $sql = "SELECT $cols FROM comida where com_situacion = 1 ";
 
-        if($this->cli_nombre != ''){
-            $sql .= " AND cli_nombre like '%$this->cli_nombre%' ";
+        if($this->com_nombre != ''){
+            $sql .= " AND com_nombre like '%$this->com_nombre%' ";
         }
-        if($this->cli_apellido != ''){
-            $sql .= " AND cli_apellido = $this->cli_apellido ";
-        }
-
-        if($this->cli_nit != ''){
-            $sql .= " AND cli_nit = $this->cli_nit ";
+        if($this->com_menu != ''){
+            $sql .= " AND com_menu = $this->com_menu ";
         }
 
-        if($this->cli_telefono != ''){
-            $sql .= " AND cli_telefono = $this->cli_telefono ";
+        if($this->com_fecha != ''){
+            $sql .= " AND com_fecha = $this->com_fecha ";
+        }
+
+        if($this->com_tiempo != ''){
+            $sql .= " AND com_tiempo = $this->com_tiempo ";
+        }
+        if($this->com_sirvio != ''){
+            $sql .= " AND com_sirvio = $this->com_sirvio ";
         }
 
 
--- vistas/comida/index.php
+++ vistas/comida/index.php
@@ -5,32 +5,32 @@
     <form action="../../../parcial_is2024/controladores/comida/guardar.php" method="POST" class="border bg-light shadow rounded p-4 col-lg-6">
         <div class="row mb-3">
             <div class="col">
-                <label for="cli_nombre">NOMBRE QUIEN RECIBIO LA COMIDA</label>
-                <input type="text" name="cli_nombre" id="cli_nombre" class="form-control" placeholder="Juan Perez"required>
+                <label for="com_nombre">NOMBRE QUIEN RECIBIO LA COMIDA</label>
+                <input type="text" name="com_nombre" id="com_nombre" class="form-control" placeholder="Juan Perez"required>
             </div>
         </div>
         <div class="row mb-3">
             <div class="col">
-                <label for="cli_apellido">NOMBRE DEL MENU</label>
-                <input type="text" name="cli_apellido" id="cli_apellido" class="form-control" required>
+                <label for="com_menu">NOMBRE DEL MENU</label>
+                <input type="text" name="com_menu" id="com_menu" class="form-control" required>
             </div>
         </div>
         <div class="row mb-3">
             <div class="col">
-                <label for="cli_nit">FECHA Y HORA</label>
-                <input type="datetime-local" name="cli_nit" id="cli_nit" class="form-control" required>
+                <label for="com_fecha">FECHA Y HORA</label>
+                <input type="datetime-local" name="com_fecha" id="com_fecha" class="form-control" required>
             </div>
         </div>
         <div class="row mb-3">
             <div class="col">
-                <label for="cli_nit">TIEMPO DE COMIDA</label>
-                <input type="text" name="cli_nit" id="cli_nit" class="form-control" required>
+                <label for="com_tiempo">TIEMPO DE COMIDA</label>
+                <input type="text" name="com_tiempo" id="com_tiempo" class="form-control" required>
             </div>
         </div>
         <div class="row mb-3">
             <div class="col">
-                <label for="cli_nit">NOMBRE QUIEN SIRVIO</label>
-                <input type="datetime" name="cli_nit" id="cli_nit" class="form-control" required>
+                <label for="com_sirvio">NOMBRE QUIEN SIRVIO</label>
+                <input type="datetime" name="com_sirvio" id="com_sirvio" class="form-control" required>
             </div>
         </div>
 
